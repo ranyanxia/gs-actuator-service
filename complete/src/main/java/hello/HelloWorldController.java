32019fd7d47cf04ddc4d71928dd7dd89caff4588
ranyanxia <ranyanxia20@163.com>
2018-05-14 20:43:17 +0800

    add log

--- complete/src/main/java/hello/HelloWorldController.java
+++ complete/src/main/java/hello/HelloWorldController.java
@@ -2,6 +2,8 @@ package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
+    private static Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 
     private static final String template = "Hello, %s! ";
     private final AtomicLong counter = new AtomicLong();
@@ -21,7 +24,8 @@ public class HelloWorldController {
 
     @GetMapping("/sayhi")
     @ResponseBody
-    public Greeting syHi(@RequestParam(name = "name", required = false, defaultValue = "Stranger") String name) {
+    public Greeting sayHi(@RequestParam(name = "name", required = false, defaultValue = "Stranger") String name) {
+        logger.info("{} is comming, say hi to him/her", name);
         return new Greeting(counter.incrementAndGet(), String.format("Hi, %s ~~~", name));
     }
 
