fc3933ec72ec81bf8cdf981481e38ca9c0abbbc3
ranyanxia <ranyanxia20@163.com>
2018-05-15 21:17:17 +0800

    change script

--- complete/src/script/deploy.sh
+++ complete/src/script/deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # copy from target to app folder
-APP_HOME=/home/ec2-user/apps/
+APP_HOME=/var/royapps/
 APP_NAME='gs-actuator-service'
 LOG_PATH=$APP_HOME$APP_NAME.log
 
 echo "deploying app ${APP_NAME}"
-mv /home/ec2-user/apps/gs-actuator-service-0.1.0.jar  /home/ec2-user/apps/gs-actuator-service-0.1.0.jar.bak
-cp /var/lib/jenkins/workspace/roy_demo_mvn_github/complete/target/gs-actuator-service-0.1.0.jar /home/ec2-user/apps/
+mv $APP_HOME/gs-actuator-service-0.1.0.jar  $APP_HOME/gs-actuator-service-0.1.0.jar.bak
+cp /var/lib/jenkins/workspace/roy_demo_mvn_github/complete/target/gs-actuator-service-0.1.0.jar $APP_HOME
 
 
 cd $APP_HOME
@@ -23,6 +23,8 @@ else
 fi
 
 echo 'starting the app'
-nohup java -server -jar ${APP_NAME}-0.1.0.jar > $LOG_PATH 2>&1 &
+#nohup java -server -jar ${APP_NAME}-0.1.0.jar > $LOG_PATH 2>&1
+nohup java -server -jar ${APP_NAME}-0.1.0.jar >$LOG_PATH 2>&1 &
 
+chmod 777 $LOG_PATH
 echo "app started, log path: ${LOG_PATH}"
